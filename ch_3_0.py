82d0c18838447319406e491194c8b568ee67eddf
DmEremin <dm.eremin271090@gmail.com>
2021-05-07 22:14:31 +0300

    some changes in LinkedList's methods

--- ch_3_0.py
+++ ch_3_0.py
@@ -1,78 +1,39 @@
-class Node:
+class Cell:
     def __init__(self, value=None):
         self.value = value
-        self.next_node = None
+        self.next = None
 
 
 class LinkedList:
     def __init__(self):
-        self.head = None
+        self.top = Cell(None)  # limiter
 
-    def is_contain(self, value):
-        current_node = self.head
-        while current_node:
-            if value == current_node.value:
-                return True
-            else:
-                current_node = current_node.next_node
-        return False
+    def add_at_beginning(self, new_cell):
+        new_cell.next = self.top.next
+        self.top.next = new_cell
 
-    def add_node(self, new_value):
-        new_node = Node(new_value)
-        if not self.head:
-            self.head = new_node
-            return
-        current_node = self.head
-        while current_node.next_node:
-            current_node = current_node.next_node
-        current_node.next_node = new_node
+    def add_at_end(self, new_cell):
+        current_cell = self.top
+        while current_cell.next:
+            current_cell = current_cell.next
+        current_cell.next = new_cell
 
-    def get(self, value_index):
-        current_node = self.head
-        current_index = 0
-        while current_node:
-            if current_index == value_index:
-                return current_node.value
-            elif current_node.next_node:
-                current_index += 1
-                current_node = current_node.next_node
-            else:
-                return 'Index out of range'
-        return 'Linked list is empty'
+    @staticmethod
+    def insert_cell(after_me, new_cell):
+        new_cell.next = after_me.next
+        after_me.next = new_cell
 
-    def remove_node(self, remove_index):
-        current_node = self.head
-        current_index = 0
-
-        while current_node:
-            if remove_index == 0:
-                if current_node.next_node:
-                    self.head = current_node.next_node
-                else:
-                    self.head = None
-                break
-
-            if current_index == remove_index:
-                previous_node.next_node = current_node.next_node
-                break
-            elif current_node.next_node:
-                previous_node = current_node
-                current_node = current_node.next_node
-                current_index += 1
-            else:
-                return 'Index out of range'
+    @staticmethod
+    def delete_after(after_me):
+        after_me.next = after_me.next.next
 
     def show(self):
-        current_node = self.head
+        current_cell = self.top
         current_index = 0
         print('LINKED LIST')
         print('----------')
-        while current_node:
-            print(f'{current_index}: {current_node.value}')
-            if current_node.next_node:
-                current_node = current_node.next_node
-                current_index += 1
-            else:
-                print('----------')
-                return
+        while current_cell:
+            print(f'{current_index}: {current_cell.value}')
+            current_cell = current_cell.next
+            current_index += 1
         print('----------')
