86ea69676d2ee0dad0b30dfe7bdf935a68b748c7
eremin <dm.eremin271090@gmail.com>
2021-05-27 15:46:16 +0300

    fix task 11.2 chapter 3

--- ch_3_11.2.py
+++ ch_3_11.2.py
@@ -18,28 +18,29 @@ class LinkedList:
     def __init__(self):
         self.top = Cell(Planet())  # sentinel
 
+    def add_link_to_planet(self, new_planet, next_link, attr):
+        new_planet = Cell(new_planet)
+        current_planet = self.top
+        while getattr(current_planet, next_link) and getattr(getattr(current_planet, next_link).value, attr) < \
+                getattr(new_planet.value, attr):
+            current_planet = getattr(current_planet, next_link)
+        setattr(new_planet, next_link, getattr(current_planet, next_link))
+        setattr(current_planet, next_link, new_planet)
+
     def add_planet_to_list(self, new_planet):
-        current_planet = self.top
-        while current_planet.next_distance and current_planet.next_distance.value.distance < new_planet.value.distance:
-            current_planet = current_planet.next_distance
-        new_planet.next_distance = current_planet.next_distance
-        current_planet.next_distance = new_planet
-
-        current_planet = self.top
-        while current_planet.next_mass and current_planet.next_mass.value.mass < new_planet.value.mass:
-            current_planet = current_planet.next_mass
-        new_planet.next_mass = current_planet.next_mass
-        current_planet.next_mass = new_planet
-
-        current_planet = self.top
-        while current_planet.next_diameter and current_planet.next_diameter.value.diameter < new_planet.value.diameter:
-            current_planet = current_planet.next_diameter
-        new_planet.next_diameter = current_planet.next_diameter
-        current_planet.next_diameter = new_planet
+        LinkedList.add_link_to_planet(self, new_planet, 'next_distance', 'distance')
+        LinkedList.add_link_to_planet(self, new_planet, 'next_mass', 'mass')
+        LinkedList.add_link_to_planet(self, new_planet, 'next_diameter', 'diameter')
 
     def sorted_by(self, parameter):
         current_planet = self.top
-        current_index = 0
+        if parameter == 'DISTANCE':
+            current_planet = current_planet.next_distance
+        elif parameter == 'MASS':
+            current_planet = current_planet.next_mass
+        elif parameter == 'DIAMETER':
+            current_planet = current_planet.next_diameter
+        current_index = 1
         print(f'LIST OF PLANETS SORTED BY {parameter}:')
         print('----------')
         while current_planet:
@@ -58,9 +59,9 @@ class LinkedList:
 
 
 sample = LinkedList()
-sample.add_planet_to_list(Cell(Planet('Mercury', 1, 1, 4)))
-sample.add_planet_to_list(Cell(Planet('Venus', 2, 3, 5)))
-sample.add_planet_to_list(Cell(Planet('Earth', 3, 2, 3)))
+sample.add_planet_to_list(Planet('Mercury', 1, 1, 4))
+sample.add_planet_to_list(Planet('Venus', 2, 3, 5))
+sample.add_planet_to_list(Planet('Earth', 3, 2, 3))
 sample.sorted_by('DISTANCE')
 sample.sorted_by('MASS')
 sample.sorted_by('DIAMETER')
