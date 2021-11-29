01929c6b9bd43b976453745e93251c9740766faf
ErmakFPG <dm.eremin271090@gmail.com>
2021-11-29 14:15:11 +0300

    Create ch_5_8.py
    added ch_5_8

---
+++ ch_5/ch_5_8.py
@@ -0,0 +1,40 @@
+class Cell:
+    def __init__(self, value=None, priority=0):
+        self.value = value
+        self.next = None
+        self.priority = priority
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.top = Cell()  # sentinel
+
+    def enqueue(self, new_cell):
+        new_cell.next = self.top.next
+        self.top.next = new_cell
+
+    def dequeue(self):
+        if self.top.next is None:
+            raise IndexError
+
+        current_cell = delete_after = self.top
+        top_priority = 0
+
+        while current_cell.next:
+            if current_cell.next.priority >= top_priority:
+                delete_after = current_cell
+                top_priority = delete_after.next.priority
+            current_cell = current_cell.next
+
+        delete_after.next = delete_after.next.next
+
+    def show(self):
+        current_cell = self.top
+        current_index = 0
+        print('LINKED LIST')
+        print('----------')
+        while current_cell:
+            print(f'{current_index}: {current_cell.value} (priority = {current_cell.priority})')
+            current_cell = current_cell.next
+            current_index += 1
+        print('----------')
