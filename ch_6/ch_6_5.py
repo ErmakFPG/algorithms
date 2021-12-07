d2fe7a1d184b061fd6883bb42e2984f4d2e8d392
ErmakFPG <dm.eremin271090@gmail.com>
2021-12-07 14:57:28 +0300

    Create ch_6_5.py

---
+++ ch_6/ch_6_5.py
@@ -0,0 +1,25 @@
+from custom_list import CustomList
+
+
+def bubble_sort(array):
+    not_sorted = True
+    while not_sorted:
+        not_sorted = False
+        for i in range(array.get_size() - 1):
+            if array.get_value(i) > array.get_value(i + 1):
+                tamp = array.get_value(i)
+                array.set_value(i, array.get_value(i + 1))
+                array.set_value(i + 1, tamp)
+                not_sorted = True
+
+
+a = CustomList(6)
+a.add_value(4)
+a.add_value(1)
+a.add_value(3)
+a.add_value(7)
+a.add_value(5)
+a.add_value(2)
+a.show_collection()
+bubble_sort(a)
+a.show_collection()
