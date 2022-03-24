e338d939119fa20825b72de0924663bfcf0dce41
ErmakFPG <dm.eremin271090@gmail.com>
2022-03-24 16:47:12 +0300

    Update ch_6_5.py

--- ch_6/ch_6_5.py
+++ ch_6/ch_6_5.py
@@ -13,13 +13,14 @@ def bubble_sort(array):
                 not_sorted = True
 
 
-a = CustomList(6)
-a.add_value(4)
-a.add_value(1)
-a.add_value(3)
-a.add_value(7)
-a.add_value(5)
-a.add_value(2)
-a.show_collection()
-bubble_sort(a)
-a.show_collection()
+if __name__ == '__main__':
+    a = CustomList(6)
+    a.add_value(4)
+    a.add_value(1)
+    a.add_value(3)
+    a.add_value(7)
+    a.add_value(5)
+    a.add_value(2)
+    a.show_collection()
+    bubble_sort(a)
+    a.show_collection()
